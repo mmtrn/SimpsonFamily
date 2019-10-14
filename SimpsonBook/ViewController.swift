c65a3af33837f148e70d6ecfac6822bc03503308
Mehmet Turan <mmtrn@outlook.com>
2019-10-14 15:55:13 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Mehmet Turan on 10/14/19.
+//  Copyright © 2019 Mehmet Turan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
